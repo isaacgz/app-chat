028f9f5636badb1591926a16db01d17ce97f59e9
Isaac Garcia <joelgarcia405@gmail.com>
2021-07-06 11:18:43 -0500

    First commit

---
+++ models/index.js
@@ -0,0 +1,20 @@
+
+
+const Categoria = require('./categoria');
+const Producto = require('./producto');
+const ChatMensajes = require('./chat-mensajes');
+const Role = require('./role');
+const Server = require('./server');
+const Usuario = require('./usuario');
+
+
+
+module.exports = {
+    Categoria,
+    Producto,
+    Role,
+    Server,
+    Usuario,
+    ChatMensajes,
+}
+
